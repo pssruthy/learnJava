c0798517d1a6debb3437d63a1ab953aea59017aa
pssruthy <58027046+pssruthy@users.noreply.github.com>
2020-10-22 12:22:16 +0530

    Added compount interest

---
+++ day1/CompoundInterest.java
@@ -0,0 +1,18 @@
+// Compound Interest - Write a program that calculates the compound interest on a given sum with a rate of interest over a given time period.
+// Input: 3 numbers (principal, rate of interest, period)
+
+public class CompoundInterest {
+  public static void main(String[] args) {
+    int principal = Integer.parseInt(args[0]);
+    double interestRate = Integer.parseInt(args[1]) / 100.0;
+    int period = Integer.parseInt(args[2]);
+
+    double totalAmount = principal;
+    for(int count = 1;count <= period; count++){
+      totalAmount *= (1 + interestRate);
+    }
+
+    double compoundInterest = totalAmount - principal;
+    System.out.println(compoundInterest);
+  }
+}
