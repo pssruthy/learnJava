2581a505208424cca1589155d2373b8ea24c6636
pssruthy <58027046+pssruthy@users.noreply.github.com>
2020-10-24 15:47:16 +0530

    Added lastIndexOf and reverse of an int array

--- day3-array/ArrayMethods.java
+++ day3-array/ArrayMethods.java
@@ -1,4 +1,11 @@
 public class ArrayMethods {
+  public static void printArray(int[] array) {
+
+    for (int i : array) {
+      System.out.println(i);
+    }
+  }
+
   public static int indexOf(int number, int[] numbers) {
 
     for(int index = 0; index < numbers.length; index++) {
@@ -9,8 +16,29 @@ public class ArrayMethods {
     return -1;
   }
 
+  public static int lastIndexOf(int number, int[] numbers) {
+    int numberAt = -1;
+
+    for(int index = 0; index < numbers.length; index++) {
+      if(number == numbers[index]) {
+        numberAt = index;
+      }
+    }
+    return numberAt;
+  }
+
+  public static int[] reverse(int[] numbers) {
+    int[] reversedNumbers = new int[numbers.length];
+
+    for(int index = 0; index < numbers.length; index++) {
+      reversedNumbers[index] = numbers[numbers.length - index - 1];
+    }
+
+    return reversedNumbers;
+  }
+
   public static void main(String[] args) {
-    int[] numbers = {1, 2, 3, 4 };
-    System.out.println(indexOf(9, numbers));
+    int[] numbers = {1, 2, 3, 4 , 4, 3};
+    printArray(reverse(numbers));
   }
 }
