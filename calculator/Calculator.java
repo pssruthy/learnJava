368e78ec7a7cdc29d1ace06768216941b98854b5
pssruthy <58027046+pssruthy@users.noreply.github.com>
2020-10-30 01:19:45 +0530

    Added feature for arg length 0

--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -1,6 +1,10 @@
 public class Calculator {
   public double result;
 
+  public Calculator() {
+    this.result = 0;
+  }
+
   public Calculator(double value) {
     this.result = value;
   }
@@ -32,7 +36,7 @@ public class Calculator {
 
   public static void main(String[] args) {
 
-    Calculator calc = new Calculator(Double.parseDouble(args[0]));
+    Calculator calc = args.length == 0 ? new Calculator() : new Calculator(Double.parseDouble(args[0]));
 
     for(int index = 1; index < args.length; index += 2) {
       calc.execute(args[index], Double.parseDouble(args[index + 1]));
