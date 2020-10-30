5aa87273459869c82f310d4e466f41e3c5ea1d0d
pssruthy <58027046+pssruthy@users.noreply.github.com>
2020-10-31 01:20:57 +0530

    Added a src file and separated Main and Calculator classes

--- calculator/src/Calculator.java
+++ calculator/src/Calculator.java
@@ -33,15 +33,4 @@ public class Calculator {
   public double getResult() {
     return this.result;
   }
-
-  public static void main(String[] args) {
-
-    Calculator calc = args.length == 0 ? new Calculator() : new Calculator(Double.parseDouble(args[0]));
-
-    for(int index = 1; index < args.length; index += 2) {
-      calc.execute(args[index], Double.parseDouble(args[index + 1]));
-    }
-
-    System.out.println(calc.getResult());
-  }
 }
